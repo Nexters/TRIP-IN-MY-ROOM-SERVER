a0d10e6090d40c316b32b1e20c06832a8a8deaa2
baeji.hoon <baeji.hoon@linecorp.com>
2021-01-13 22:00:40 +0900

    project init

---
+++ src/main/kotlin/com/trip/in/my/room/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package com.trip.in.my.room.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerApplication>(*args)
+}
