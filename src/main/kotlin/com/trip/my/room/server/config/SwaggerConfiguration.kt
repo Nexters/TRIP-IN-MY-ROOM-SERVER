eb30f57c34dbcb9e6aa21b0472c842bab75a3651
baeji <hahawjstk@gmail.com>
2021-01-21 00:36:56 +0900

    Add swagger uiConfiguration and UserController

--- src/main/kotlin/com/trip/my/room/server/config/SwaggerConfiguration.kt
+++ src/main/kotlin/com/trip/my/room/server/config/SwaggerConfiguration.kt
@@ -6,6 +6,9 @@ import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger.web.OperationsSorter
+import springfox.documentation.swagger.web.UiConfiguration
+import springfox.documentation.swagger.web.UiConfigurationBuilder
 
 @Configuration
 class SwaggerConfiguration {
@@ -18,11 +21,18 @@ class SwaggerConfiguration {
             .apiInfo(this.getSwaggerServerInfo())
     }
 
-    private fun getSwaggerServerInfo(): ApiInfo {
+    private fun getSwaggerServerInfo(): ApiInfo? {
         return ApiInfoBuilder()
-            .title("Swagger")
-            .description("Trip in my room")
+            .title("Trip in my room")
             .version("0.0.1")
             .build()
     }
+
+    @Bean
+    fun uiConfiguration(): UiConfiguration? {
+        return UiConfigurationBuilder.builder()
+            .operationsSorter(OperationsSorter.METHOD)
+            .build()
+    }
+
 }
