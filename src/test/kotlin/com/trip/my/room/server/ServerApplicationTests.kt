d265de7a59712c89c0354afd7a50b3f3325cb8c0
baeji <hahawjstk@gmail.com>
2021-01-23 17:11:01 +0900

    Update dependency releated to test

--- src/test/kotlin/com/trip/my/room/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/trip/my/room/server/ServerApplicationTests.kt
@@ -1,13 +1,15 @@
 package com.trip.my.room.server
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ServerApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun `contextLoads`() {
+        assertTrue(true)
+    }
 
 }
