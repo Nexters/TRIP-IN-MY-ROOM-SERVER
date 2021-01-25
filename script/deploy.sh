efe176e39749d3bc578ec13dffc2d6559cb06285
Ji Hoon Bae <hahawjstk@gmail.com>
2021-01-25 23:53:13 +0900

    #13 deploy.sh 버그 수정 (#15)
    * Add making unique nohup file script
    
    * Add now datetime variable in deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,7 @@
 
 REPOSITORY=/home/ubuntu/back-end/product
 PROJECT_NAME=tripinmyroom
+NOW_DATETIME=`date +"%Y-%m-%dT%T"`
 
 CURRENT_PID=$(pgrep java)
 echo "> 이전 product pid $CURRENT_PID"
@@ -21,6 +22,6 @@ echo "> JAR name: $JAR_NAME"
 
 nohup java -jar \
     -Dspring.profiles.active=release \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup-$NOW_DATETIME.out 2>&1 &
 
 echo "> Deployment finished."
