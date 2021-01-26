fb5e476ff10c4a871169e7346ee97413e0dcbbb8
Daim Tak <histuckyi@gmail.com>
2021-01-27 00:28:49 +0900

    Update build.gradle.kts - Add Mysql, jpa, jdbc dependency - Set Allopen (entity, MappledSuperclass, Embeddable) - Set noArg entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
+	kotlin("plugin.allopen") version "1.4.0"
+	kotlin("plugin.noarg") version "1.4.0"
 }
 
 group = "com.trip.in.my.room"
@@ -18,16 +20,34 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
-
-	implementation ("io.springfox:springfox-swagger2:2.9.2")
-	implementation ("io.springfox:springfox-swagger-ui:2.9.2")
-
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// swagger
+	implementation ("io.springfox:springfox-swagger2:2.9.2")
+	implementation ("io.springfox:springfox-swagger-ui:2.9.2")
+	
+	// DB
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("mysql:mysql-connector-java")
 }
+
+// kotlin에서는 클래스와 프로퍼티, 함수가 기본적으로 final이므로 상속이 가능하도록 자동 open 설정
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
+// hibernate에서는 인자가 없는 생성자가 필요하므로 자동으로 기본 생성자를 생성하도록 설정
+noArg {
+	annotation("javax.persistence.Entity")
+}
+
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
