21039fab71c5e05a823b13ee483edc96d164af27
Daim Tak <histuckyi@gmail.com>
2021-01-31 03:02:35 +0900

    Add login template with thymeleaf

--- src/main/kotlin/com/trip/my/room/server/user/LoginController.kt
+++ src/main/kotlin/com/trip/my/room/server/user/LoginController.kt
@@ -1,14 +1,18 @@
 package com.trip.my.room.server.user
 
+import com.trip.my.room.server.config.MyConfigurationProperties
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class LoginController {
+class LoginController(@Autowired val myConfig: MyConfigurationProperties) {
 	
 	@GetMapping("")
 	fun loginPage(model: Model): String {
+		model.addAttribute("client_id", myConfig.clientId)
+		model.addAttribute("redirectUri", myConfig.redirectUrl)
 		return "login"
 	}
 }
