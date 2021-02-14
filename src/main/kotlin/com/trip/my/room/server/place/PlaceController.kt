d9e23d21037cbe21d1bb917bd8c95fa786c81af5
Daim Tak <histuckyi@gmail.com>
2021-02-14 15:43:42 +0900

    Fix place error (#33)

--- src/main/kotlin/com/trip/my/room/server/place/PlaceController.kt
+++ src/main/kotlin/com/trip/my/room/server/place/PlaceController.kt
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
-import java.util.*
-import javax.validation.Valid
 
 @RestController
 @RequestMapping("/place")
