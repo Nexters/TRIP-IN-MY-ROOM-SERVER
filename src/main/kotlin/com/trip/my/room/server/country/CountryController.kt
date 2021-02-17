d8ab8dd1adcc06a3854f336a8c4c4be10c2b5fbe
Daim Tak <histuckyi@gmail.com>
2021-02-18 05:16:41 +0900

    Update add default country data into DB

--- src/main/kotlin/com/trip/my/room/server/country/CountryController.kt
+++ src/main/kotlin/com/trip/my/room/server/country/CountryController.kt
@@ -1,19 +1,25 @@
 package com.trip.my.room.server.country
 
 import com.trip.my.room.server.user.IfUserPrincipal
+import io.swagger.annotations.ApiOperation
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/countries")
 class CountryController(private val countryService: CountryService) {
-
-    @GetMapping("/stories")
-    fun getPlaceWithStoryCount(@AuthenticationPrincipal principal: IfUserPrincipal): List<CountryStoryCountResponseDto> {
-        val userId = principal.getUserUUID()
-
-        return countryService.getPlaceWithStoryCount(userId)
-    }
+	
+	@ApiOperation("country 정보 가져오기 (others 옵션으로 내가 설정한 country함께 조회)")
+	@GetMapping
+	fun getCountriesByUserId(@AuthenticationPrincipal principal: IfUserPrincipal, @RequestParam("others", defaultValue = "false") others: Boolean): List<CountryDto.CountryOut> {
+		return countryService.getCountriesByUserId(principal.getUserUUID(), others)
+	}
+	
+	@GetMapping("/stories")
+	fun getPlaceWithStoryCount(@AuthenticationPrincipal principal: IfUserPrincipal): List<CountryStoryCountResponseDto> {
+		return countryService.getPlaceWithStoryCount(principal.getUserUUID())
+	}
 }
--- src/main/kotlin/com/trip/my/room/server/country/CountryRepositoryImpl.kt
+++ src/main/kotlin/com/trip/my/room/server/country/CountryRepositoryImpl.kt
@@ -5,11 +5,21 @@ import com.trip.my.room.server.country.QCountryEntity.countryEntity
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.*
 
-class CountryRepositoryImpl(@Autowired private val query: JPAQueryFactory): CountryRepositoryCustom {
+class CountryRepositoryImpl(@Autowired private val query: JPAQueryFactory) : CountryRepositoryCustom {
 	
 	override fun finCountryById(countryId: UUID): CountryEntity? {
 		return query.selectFrom(countryEntity)
 				.where(countryEntity.id.eq(countryId))
 				.fetchOne()
+	}
+	
+	override fun getCountriesByUserId(userId: UUID, others: Boolean): MutableList<CountryEntity> {
+		var query = query.selectFrom(countryEntity)
+		if (others) {
+			// others도 포함이면 userId로 생성된 country와 type이 other가 아닌 모든 것들을 포함함
+			query.where(countryEntity.user.id.eq(userId).or(countryEntity.type.notIn("others")))
+		}
+		val result = query.fetchResults()
+		return result.results
 	}
 }
