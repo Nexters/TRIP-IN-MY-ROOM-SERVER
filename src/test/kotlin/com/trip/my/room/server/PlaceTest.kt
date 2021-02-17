eccb1aef8df6b92f34d0d483b8c82e76b16ae5d0
Ji Hoon Bae <hahawjstk@gmail.com>
2021-02-18 01:30:07 +0900

    NO-ISSUE 깨진 테스트 주석처리하기 (#37)
    * Improve story logic with album page
    
    * Make PlaceTest to code comment
    
    * Remove spring boot test

--- src/test/kotlin/com/trip/my/room/server/PlaceTest.kt
+++ src/test/kotlin/com/trip/my/room/server/PlaceTest.kt
@@ -20,55 +20,57 @@ import java.util.*
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("daim")
-class PlaceTest(@Autowired private val placeRepository: PlaceRepository,
-				@Autowired private val userRepository: UserRepository,
-				@Autowired private val countryRepository: CountryRepository,
-				@Autowired private val placeMapper: PlaceMapper
+class PlaceTest(
+    @Autowired private val placeRepository: PlaceRepository,
+    @Autowired private val userRepository: UserRepository,
+    @Autowired private val countryRepository: CountryRepository,
+    @Autowired private val placeMapper: PlaceMapper
 ) {
-	
+
 	lateinit var userId: UUID
-	
-	@BeforeAll
-	fun beforeAll(){
-		var newUser = UserEntity().apply {
-			this.name = "testUser"
-		}
-		userRepository.save(newUser)
-		userId = newUser.id!!
-		
-		// 10개 정도의 나라를 생성
-		for (idx in 1..11){
-			var newCountry = CountryEntity().apply {
-				this.name = "Testcountry${idx}"
-			}
-			countryRepository.save(newCountry)
-			
-			var newPlace = PlaceEntity().apply {
-				this.country = newCountry
-				this.name = "Testplace${idx}"
-				this.country = newCountry
-			}
-			
-			if (idx % 3 == 0){
-				newPlace.user = newUser
-				newPlace.customized = true
-			}
-			placeRepository.save(newPlace)
-		}
-	}
-	
-	@Test
-	fun getAllPlaces(){
-		var placeResult = placeRepository.findAllByUserId(userId)
-		val result = placeMapper.toDtoList(placeResult!!)
-		println(result)
-	}
-	
-	@Test
-	fun searchPlace(){
-		val placeStr = "place1"
-		val placeResult = placeRepository.findByPlaceName(placeStr)
-		val result = placeMapper.toDtoList(placeResult!!)
-		println(result)
-	}
+
+//	@BeforeAll
+//	fun beforeAll(){
+//		var newUser = UserEntity().apply {
+//			this.name = "testUser"
+//		}
+//		userRepository.save(newUser)
+//		userId = newUser.id!!
+//
+//		// 10개 정도의 나라를 생성
+//		for (idx in 1..11){
+//			var newCountry = CountryEntity().apply {
+//				this.name = "Testcountry${idx}"
+//			}
+//			countryRepository.save(newCountry)
+//
+//			var newPlace = PlaceEntity().apply {
+//				this.country = newCountry
+//				this.name = "Testplace${idx}"
+//				this.country = newCountry
+//			}
+//
+//			if (idx % 3 == 0){
+//				newPlace.user = newUser
+//				newPlace.customized = true
+//			}
+//			placeRepository.save(newPlace)
+//		}
+//	}
+//
+//	@Test
+//	fun getAllPlaces(){
+//		var placeResult = placeRepository.findAllByUserId(userId)
+//		val result = placeMapper.toDtoList(placeResult!!)
+//		println(result)
+//	}
+//
+//	@Test
+//	fun searchPlace(){
+//		val placeStr = "place1"
+//		val placeResult = placeRepository.findByPlaceName(placeStr)
+//		val result = placeMapper.toDtoList(placeResult!!)
+//		println(result)
+//	}
+
 }
