1014aec6c798459b52240ce69ac3870ee81b1a27
Daim Tak <histuckyi@gmail.com>
2021-02-20 05:05:15 +0900

    Fix naver login token api

--- src/main/kotlin/com/trip/my/room/server/user/service/SocialLoginService.kt
+++ src/main/kotlin/com/trip/my/room/server/user/service/SocialLoginService.kt
@@ -23,18 +23,18 @@ class SocialLoginService(private val kakaoClientService: KakaoClientService,
 		if (SocialType.valueOf(socialType.toUpperCase()).equals(SocialType.KAKAO)) {
 			return kakaoClientService.getUserExtraInfo(tokenType, token)
 		}
-		if (SocialType.valueOf(socialType.toUpperCase()).equals(SocialType.KAKAO)) {
+		if (SocialType.valueOf(socialType.toUpperCase()).equals(SocialType.NAVER)) {
 			return naverClientService.getUserExtraInfo(tokenType, token)
 		}
 		throw InvalidParameterException("유효하지 않은 socialType 입니다. : ${socialType}")
 	}
 	
-	fun unlinkWithSocial(socialType: String, user: UserEntity, access_token : String ?= null): Boolean {
+	fun unlinkWithSocial(socialType: String, user: UserEntity, access_token: String? = null): Boolean {
 		if (SocialType.valueOf(socialType.toUpperCase()).equals(SocialType.KAKAO)) {
 			return kakaoClientService.unlinkWithKakao(user.socialId!!)
 		} else if (SocialType.valueOf(socialType.toUpperCase()).equals(SocialType.NAVER)) {
 			// 네이버 추가
-			if (access_token == null){
+			if (access_token == null) {
 				return false
 			}
 			return naverClientService.unlinkFromNaver(user.socialId!!, access_token)
